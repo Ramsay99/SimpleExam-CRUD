70ae9eac958826b4b4e166918806ea0db3869985
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-10-26 19:48:06 +0300

    Update user.dart

--- simpleexam/lib/user/user.dart
+++ simpleexam/lib/user/user.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: non_constant_identifier_names
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 
 const String firebaseCollectionName = 'users';
+final CollectionReference<Map<String, dynamic>> usersCollection =
+    FirebaseFirestore.instance.collection(firebaseCollectionName);
 const String firebaseUserID = 'id';
 const String firebaseUserName = 'name';
 const String firebaseUserAge = 'age';
@@ -25,19 +24,10 @@ class User {
   static Stream<List<User>> getUsers() {
     return FirebaseFirestore.instance
         .collection(firebaseCollectionName)
+        .orderBy('accCreated', descending: true)
         .snapshots()
         .map((snapshot) =>
             snapshot.docs.map((doc) => User.fromJson(doc.data())).toList());
-  }
-
-  static Widget displayUser_ListTile(User user) {
-    bool isSelected = false;
-    return ListTile(
-      isThreeLine: true,
-      leading: CircleAvatar(child: Text('${user.age}')),
-      title: Text(user.name),
-      subtitle: Text("${user.accCreated.toIso8601String()}\n${user.id}"),
-    );
   }
 
   Map<String, dynamic> toJson() {
@@ -58,5 +48,21 @@ class User {
           ? DateTime(1)
           : (jsonData[firebaseUserAccCreated] as Timestamp).toDate()),
     );
+  }
+
+  DocumentReference<Map<String, dynamic>> getID() {
+    return usersCollection.doc(id);
+  }
+
+  void setName(String name) {
+    getID().update({firebaseUserName: name});
+  }
+
+  void setAge(int age) {
+    getID().update({firebaseUserName: name});
+  }
+
+  String getAccCreated() {
+    return accCreated.toIso8601String();
   }
 }
