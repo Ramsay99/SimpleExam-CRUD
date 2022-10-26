e60ed5aa193cf31d9340cdda15f1e0eb3add10d9
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-10-26 19:46:54 +0300

    Update route_generator.dart
    minor changes

--- simpleexam/lib/route_generator/route_generator.dart
+++ simpleexam/lib/route_generator/route_generator.dart
@@ -3,9 +3,11 @@ import 'package:simpleexam/route_generator/error_route.dart';
 import 'package:simpleexam/screen/create_user.dart';
 import 'package:simpleexam/screen/crud_home.dart';
 
+import '../screen/edit_user.dart';
+
 /// This class is used to navigate around screens in a way suitable for larger apps.
 /// By creating a RouteGenerator which can encapsulate all of the routing logic - sparing code duplication.
-/// 
+///
 /// As creating many smaller classes with a certain purpose is always a good way to simplify code,
 /// and when it comes to routing, this principle still holds true.
 ///
@@ -23,21 +25,21 @@ class RouteGenerator {
     // Getting arguments passed in while calling Navigator.pushNamed
     final args = settings.arguments;
 
+    // Validation of correct data type
+    // if (args is String) {
+    //    return MaterialPageRoute(
+    //    builder: (_) => const anyPage(data: args,),
+    // }
+    // else If [args] is not of the correct type, return an error page.
+    // You can also throw an exception while in development.
+    // return errorPage();
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => const CRUDHome());
       case '/createuser':
-        // Validation of correct data type
-        // if (args is String) {
         return MaterialPageRoute(
-          builder: (_) => const CreateUser(
-              // data: args,
-              ),
+          builder: (_) => const CreateUser(),
         );
-      // }
-      // If args is not of the correct type, return an error page.
-      // You can also throw an exception while in development.
-      // return errorRoute();
       default:
         // If there is no such named route in the switch statement, e.g. /third
         return errorRoute();
